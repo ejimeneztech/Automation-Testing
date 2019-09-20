355983d196e95c18d0ae4bd1c38acfb3cf8c6d43
ejimeneztech <emiliojimeneztech@gmail.com>
2019-09-20 09:11:32 -0700

    Account Creation automation files

---
+++ test_onboarding.py
@@ -0,0 +1,102 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+print("Enter account email:")
+text = input()
+
+browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+browser.get('https://www.abcmouse.com')
+
+linkElem = browser.find_element_by_tag_name('login-button')
+linkElem.click()
+
+emailElem = browser.find_element_by_class_name('login_email')
+emailElem.send_keys(text)
+passwordElem = browser.find_element_by_class_name('login_password')
+passwordElem.send_keys('password')
+
+loginSubmit = browser.find_element_by_class_name('submit_button')
+loginSubmit.click()
+
+time.sleep(8)
+iframe = browser.find_element_by_css_selector('#content-iframe')
+browser.switch_to.frame(iframe)
+
+browser.execute_script('document.querySelector("#survey-link").click()')
+
+time.sleep(4)
+
+browser.execute_script('document.querySelector("#survey-2-link").click()')
+
+time.sleep(4)
+
+browser.execute_script('document.querySelector("#setup-parent-link").click()')
+
+time.sleep(4)
+
+FNinput = browser.find_element_by_name('firstName')
+FNinput.send_keys('Parent')
+famName = browser.find_element_by_name('familyName')
+famName.send_keys('Test')
+
+time.sleep(4)
+browser.execute_script('document.querySelector("#setup-parent-continue").click()')
+
+time.sleep(4)
+kidName = browser.find_element_by_name('firstName')
+kidName.send_keys('Kid 1')
+
+browser.execute_script('document.querySelector("#onboarding > div > div > div.onboarding-content > form > div.first-row > div.child-gender.prevent-stretch > div > div.aofl-radio.boy > label").click()')
+
+#Only Edit BELOW this line (Month/Year dropdowns)
+
+
+
+wrappers = browser.find_elements_by_class_name('options-wrapper')
+
+
+months = wrappers[0].find_elements_by_tag_name('aofl-option')
+
+
+years = wrappers[1].find_elements_by_tag_name('aofl-option')
+
+
+label = years[1].find_element_by_tag_name('label')
+
+browser.execute_script('document.querySelector("#onboarding > div > div > div.onboarding-content > form > div.first-row > div.child-birthday.prevent-stretch > aofl-dropdown.child-month > div > div > div.dropdown.dropdown-collapsed").click()')
+
+months[2].click()
+
+browser.execute_script('document.querySelector("#onboarding > div > div > div.onboarding-content > form > div.first-row > div.child-birthday.prevent-stretch > aofl-dropdown.child-year > div > div > div.dropdown.dropdown-collapsed").click()')
+
+
+browser.execute_script("arguments[0].click();", label)
+
+
+#Only Edit Above this line
+browser.execute_script('document.querySelector("#pre-k").click()')
+
+browser.execute_script('document.querySelector("#setup-parent-continue").click()')
+time.sleep(4)
+
+browser.execute_script('document.querySelector("#boy_avatar16").click()')
+browser.execute_script('document.querySelector("#onboarding > div > div > div > div:nth-child(5)").click()')
+time.sleep(4)
+
+browser.execute_script('document.querySelector("#hamster-1").click()')
+browser.execute_script('document.querySelector("#fish-1").click()')
+petName = browser.find_element_by_css_selector('#onboarding > div > div > div > form > div.left-content > div > div.child-hamster-name > input')
+petName.send_keys('test')
+browser.execute_script('document.querySelector("#onboarding > div > div > div > div:nth-child(4)").click()')
+time.sleep(4)
+
+browser.execute_script('document.querySelector("#onboarding > div > div > div > div.onboarding-video-container > a").click()')
+browser.execute_script('document.querySelector("#ngdialog1 > div.ngdialog-content > div.std-modal-body.with-title-bar > div.std-modal-buttons.multiple-buttons > button:nth-child(2)").click()')
+time.sleep(4)
+
+#TO SHP
+browser.execute_script('document.querySelector("#student-homepage-link").click()')
+time.sleep(4)
+browser.close()
---
+++ test_sub.py
@@ -0,0 +1,100 @@
+import time
+from selenium import webdriver
+
+print("Enter account email:")
+text = input()
+
+browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver')
+
+def expand_shadow_element(element):
+    shadow_root = browser.execute_script('return arguments[0].shadowRoot', element)
+    return shadow_root
+
+#Subscription page
+browser.get('https://www.abcmouse.com/abt/subscription')
+root1 = browser.find_element_by_tag_name('subscription-form')
+shadow_root1 = expand_shadow_element(root1)
+
+emailElem = shadow_root1.find_element_by_css_selector('#email')
+emailElem.send_keys(text)
+emailConfirmElem = shadow_root1.find_element_by_css_selector('#confirm-email')
+emailConfirmElem.send_keys(text)
+passElem = shadow_root1.find_element_by_css_selector('#password')
+passElem.send_keys('password')
+confirmPassElem = shadow_root1.find_element_by_css_selector('#confirm-password')
+confirmPassElem.send_keys('password')
+
+
+cardNameElem = shadow_root1.find_element_by_css_selector('#card-name')
+cardNameElem.send_keys('Emilio Test')
+ccNumElem = shadow_root1.find_element_by_css_selector('#cc-num')
+ccNumElem.send_keys('378750082492053')
+cvvElem = shadow_root1.find_element_by_css_selector('#cvv')
+cvvElem.send_keys('4882')
+zipElem = shadow_root1.find_element_by_css_selector('#zipcode')
+zipElem.send_keys('91203')
+
+time.sleep(10)
+
+button = shadow_root1.find_element_by_css_selector('button')
+button.click()
+
+#goes to upgrade page. Wait for page to load
+time.sleep(10)
+#expand shadow root elements
+root2 = browser.find_element_by_tag_name('landscape-upgrade-form')
+shadow_root2 = expand_shadow_element(root2)
+
+root3 = shadow_root2.find_element_by_css_selector('#no-thanks')
+shadow_root3 = expand_shadow_element(root3)
+
+noThanks = shadow_root3.find_element_by_css_selector('#radio-wrapper')
+noThanks.click()
+
+submitButton = shadow_root2.find_element_by_css_selector('#upgrade-form-container > button')
+submitButton.click()
+
+#Goes to upgrade offer pop-up. 
+root4 = browser.find_element_by_css_selector('upgrade-offer-popup')
+shadow_root4 = expand_shadow_element(root4)
+
+noUpgrade = shadow_root4.find_element_by_css_selector('upgrade-popup-redirect-button')
+noUpgrade.click()
+
+#Goes to assessment purchase page.
+time.sleep(4)
+
+root5 = browser.find_element_by_css_selector('assessment-monthly-form')
+shadow_root5 = expand_shadow_element(root5)
+
+root6 = shadow_root5.find_element_by_css_selector('abcmouse-radio-button')
+shadow_root6 = expand_shadow_element(root6)
+
+#Does not work. Clicks wrong check box. Will purchase assesment for now. Need to figure out how to click 'no thanks' checknbox
+radioButton = shadow_root6.find_element_by_css_selector('#radio-wrapper')
+radioButton.click()
+
+
+submitButton2 = shadow_root5.find_element_by_css_selector('button')
+submitButton2.click()
+
+#Goes to Assessment Monthly Thank You Pop Up
+time.sleep(3)
+
+root7 = browser.find_element_by_css_selector('assessment-monthly-thank-you-popup')
+shadow_root7 = expand_shadow_element(root7)
+
+nextButton = shadow_root7.find_element_by_css_selector('button')
+nextButton.click()
+
+#Goes to Sub-Confirm page
+time.sleep(5)
+
+root8 = browser.find_element_by_css_selector('continue-button-reg')
+shadow_root8 = expand_shadow_element(root8)
+
+contButton = shadow_root8.find_element_by_css_selector('button')
+contButton.click()
+time.sleep(4)
+browser.close()
+
