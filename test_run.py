37fe48f9a54a6083c8927fa93701d5d9a2a7ad84
your.name@aofl.com <emilio.jimenez@aofl.com>
2019-10-11 11:14:36 -0700

    reformatted

--- test_run.py
+++ test_run.py
@@ -1,4 +1,4 @@
-#This is the only file intended to run onboarding test cases
+# This is the only file intended to run onboarding test cases
 import time
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
@@ -21,12 +21,14 @@ time.sleep(8)
 iframe = browser.find_element_by_css_selector('#content-iframe')
 browser.switch_to.frame(iframe)
 
-#Test Case 1: User can click on tNc and Privacy Policy
-def test_tNc():
+
+# Test Case 1: User can click on tNc and Privacy Policy
+def test_tnc():
     try:
         browser.find_element_by_css_selector('#terms-condition')
     except:
         print('TnC link missing')
+
 
 def test_pv():
     try:
@@ -34,23 +36,27 @@ def test_pv():
     except:
         print('Privacy Policy link missing')
 
-#Test Case 2: User can click on 'Continue' in initial onboarding screen
+
+# Test Case 2: User can click on 'Continue' in initial onboarding screen
 def test_click_1():
     try:
         browser.execute_script('document.querySelector("#survey-link").click()')
     except:
         print('Continue button was not clicked')
 
+
 time.sleep(4)
 
-#Test Case 3: User can click on all radio buttons in Step 1 screen
+
+# Test Case 3: User can click on all radio buttons in Step 1 screen
 def test_survey_step_1():
     try:
         surveyItems = browser.find_elements_by_class_name('survey-item')
         for element in surveyItems:
             item = element.find_element_by_tag_name('label')
             browser.execute_script("arguments[0].click();", item)
-            surveySection2 = browser.find_element_by_css_selector('#onboarding > div > div > div > div.onboarding-survey.secondary-survey')
+            surveySection2 = browser.find_element_by_css_selector(
+                '#onboarding > div > div > div > div.onboarding-survey.secondary-survey')
             surveyItems2 = surveySection2.find_elements_by_class_name('survey-item')
             for element in surveyItems2:
                 item2 = element.find_element_by_tag_name('label')
@@ -58,7 +64,8 @@ def test_survey_step_1():
     except:
         print('Survey Options were not clicked.')
 
-#Test Case 4: User can click on continue button in Step 1 screen
+
+# Test Case 4: User can click on continue button in Step 1 screen
 def test_click_2():
     try:
         browser.execute_script('document.querySelector("#survey-2-link").click()')
